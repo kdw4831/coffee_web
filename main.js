3d44d62e5bb6499617dfaf9972fee28a9acf31a4
Deawon <19999eo@naver.com>
2023-01-21 14:50:46 +0900

    final commit

---
+++ main.js
@@ -0,0 +1,10 @@
+$(function() {
+	$('#fullpage').fullpage({
+		
+		autoScrolling:true, //스크롤 했을 때 자동으로 페이지가 넘어가는 효과
+		navigation:true,           //네비게이션 동그라미들을 생성
+		navigationPosition:'right' //네비게이션 위치를 오른쪽으로 고정시킨다.
+	});
+
+	
+});
